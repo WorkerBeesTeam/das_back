227aaaa38824046a6d82751b31e7aa8249375411
kirill@zimnikov.ru <kirill@zimnikov.ru>
2019-03-15 15:46:26 +0700

    add (sites plugin)

--- house4devices/settings/base.py
+++ house4devices/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # Django Packages
     'django_filters',
@@ -48,6 +49,8 @@ INSTALLED_APPS = [
     'applications.house',
     'applications.house_list'
 ]
+
+SITE_ID=1
 
 ROOT_URLCONF = 'house4devices.urls'
 
@@ -75,7 +78,7 @@ MIDDLEWARE = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [FRONTEND_ROOT],
+        'DIRS': [FRONTEND_ROOT, project_root + '/house4devices/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
