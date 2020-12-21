84dd1950dc395a0567636469e64a8612d79815a4
Кирилл Зимников <kirill@zimnikov.ru>
2020-12-22 00:28:25 +0700

    Value_View id is id

--- das/models.py
+++ das/models.py
@@ -406,7 +406,7 @@ class Node(Schemed_Model):
     order = models.PositiveSmallIntegerField(default=0)
 
 class Value_View(Schemed_Model):
-    type = models.OneToOneField(Device_Item_Type, on_delete=models.CASCADE, primary_key=True)
+    type = models.OneToOneField(Device_Item_Type, on_delete=models.CASCADE)
     value = models.TextField()
     view = models.TextField()
 
